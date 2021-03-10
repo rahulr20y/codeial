8748e513f2c55bafa7a9ab68adf7e38f63105e4a
Rahul Yadav <rahulr20y@gmail.com>
2021-03-10 19:18:06 +0530

    create sign-up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,7 @@
+const { user } = require("../config/mongoose");
+
+const User =require('../models/user');
+
 module.exports.profile = function(req,res){
     //res.end('<h1>User Profile</h1>');
     return res.render('user_profile',{
@@ -23,7 +27,23 @@ module.exports.signIn = function(req,res){
 //get thr sign up data
 
 module.exports.create = function(req,res){
-    //To do
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){console.log('error in finding user in signing up'); return;}
+
+        if(!user){
+            User.create(req.body,function(err,user){
+                if(err){console.log('error in creating user in signing up');return;}
+
+                return res.redirect('/users/sign-in');
+            }); 
+        }else{
+            return res.redirect('back');
+        }
+    });
 }
 
 //sign in and create a session for the user
