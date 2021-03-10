6fefb9fb5683827c624a57df86146a739c8f522e
Rahul Yadav <rahulr20y@gmail.com>
2021-03-10 15:21:51 +0530

    created sign-up and sign-in pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -18,3 +18,15 @@ module.exports.signIn = function(req,res){
         title: "Codeial | Sign In"
     });
 }
+
+
+//get thr sign up data
+
+module.exports.create = function(req,res){
+    //To do
+}
+
+//sign in and create a session for the user
+module.exports.createSession = function(req,res){
+    //TODO later
+}
