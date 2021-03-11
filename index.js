479a1f2d0f16b5e61ac75326a82158043c33fb9b
Rahul Yadav <rahulr20y@gmail.com>
2021-03-11 19:06:45 +0530

    setup SCSS

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const passportLocal = require('./config/passport-local-strategy');
 
 //const MongoStore = require('connect-mongo')(session);
 
+
+const sassMiddleware = require('node-sass-middleware');
+
+app.use(sassMiddleware({
+    src: '/assets/scss',
+    dest: '/assets/css',
+    debug: true,
+    outputStyle: 'expanded',
+    prefix: '/css'
+}));
+
 app.use(express.urlencoded());
 
 app.use(cookieParser());
