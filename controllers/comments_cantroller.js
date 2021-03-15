1788fbaec8353a6444ed1cc22773fa8a22e52734
Rahul Yadav <rahulr20y@gmail.com>
2021-03-15 19:57:35 +0530

    using ajax - comments feature are added and also noty

--- controllers/comments_cantroller.js
+++ controllers/comments_cantroller.js
@@ -13,6 +13,19 @@ module.exports.create = async function(req,res){
             });
             post.comments.push(comment);
             post.save();
+
+            if (req.xhr){
+                // Similar for comments to fetch the user's id!
+                comment = await comment.populate('user', 'name').execPopulate();
+    
+                return res.status(200).json({
+                    data: {
+                        comment: comment
+                    },
+                    message: "Post created!"
+                });
+            }
+
             req.flash('success','Comment published!');
             res.redirect('/');
         }
@@ -35,6 +48,17 @@ module.exports.destroy = async function(req,res){
             comment.remove();
     
             let post = await Post.findByIdAndUpdate(postId,{$pull:{comments: req.params.id}});
+ 
+             // send the comment id which was deleted back to the views
+             if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        comment_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+             
             req.flash('success','comment Deleted!');
             return res.redirect('back');
         }else{
@@ -46,6 +70,6 @@ module.exports.destroy = async function(req,res){
           //console.log('Error',err);
           //return;
           req.flash('error',err);
-          return res.redirect('back');
+          return;
       }
 }
