be30c38d351c305677f05e3e70a3f4a08934af06
Rahul Yadav <rahulr20y@gmail.com>
2021-03-18 19:06:59 +0530

    Authentication and Authorization

--- controllers/api/v1/users_api.js
+++ controllers/api/v1/users_api.js
@@ -15,7 +15,7 @@ module.exports.createSession = async function(req,res){
         return res.json(200,{
             message: "sign in successful, here is your token, please keep it safe",
             data: {
-                token: jwt.sign(user.toJSON(),'codeial',{expiresIn: '1000'})
+                token: jwt.sign(user.toJSON(),'codeial',{expiresIn: '10000'})
             }
         });
 
