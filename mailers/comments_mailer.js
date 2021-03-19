ab97354848e0c9e48f4f527e33290b06a058595d
Rahul Yadav <rahulr20y@gmail.com>
2021-03-19 13:19:33 +0530

    Send HTML Template Emails

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -2,13 +2,15 @@ const nodeMailer = require('../config/nodemailer');
 
 //this is another way of exporting a method
 exports.newComment = (comment) => {
-    console.log('inside new comment mailer',comment);
+    //console.log('inside new comment mailer',comment);
+
+    let htmlString = nodeMailer.renderTemplate({comment:comment},'/comments/new_comment.ejs');
 
     nodeMailer.transporter.sendMail({
         from: 'example@gmail.com',
         to: comment.user.email,
         subject: "New comment published!",
-        html: '<h1> Yup, Your comment is now published! </h1>'
+        html: htmlString
     },(err,info) => {
         if(err){console.log('Error in sending mail',err);return;}
 
